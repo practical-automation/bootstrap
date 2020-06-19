2a7ea4504cd4457050ec6154c35155b989612db7
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2020-06-19 22:54:09 +0900

    minor fix

--- files/gitlab.rb
+++ files/gitlab.rb
@@ -1,5 +1,5 @@
-external_url "http://192.168.33.10"
-registry_external_url "http://192.168.33.10:4567"
+external_url "http://192.168.77.11"
+registry_external_url "http://192.168.77.11:4567"
 gitlab_rails['time_zone'] = 'Asia/Tokyo'
 gitlab_rails['initial_root_password'] = 'password'
 gitlab_rails['initial_shared_runners_registration_token'] = 'token-AABBCCDD'
